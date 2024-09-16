037b949f563a09f4d54df086c8933472b159102b
sarahjumaiah <sarahjumaiah@gmail.com>
2024-09-16 14:24:43 +0300

    done

---
+++ main.js
@@ -0,0 +1,68 @@
+let inp = document.getElementById("inp");
+let imgUrlInput = document.getElementById("imgUrl");
+let btn = document.getElementById("btn");
+let imageContainer = document.getElementById("imageContainer"); 
+
+btn.addEventListener("click", () => {
+    if (inp.value.trim() === "" || imgUrlInput.value.trim() === "") {
+        alert("Both fields are required.");
+        return;
+    }
+
+    fetch("https://66e7e68bb17821a9d9da6e50.mockapi.io/login", {
+        method: 'POST',
+        body: JSON.stringify({
+            name: inp.value,
+            imgUrl: imgUrlInput.value 
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+    .then((response) => response.json())
+    .then(data => {
+        let container = document.createElement('div');
+        container.classList.add('col-md-4', 'mb-4');
+
+        let card = document.createElement('div');
+        card.classList.add('card'); 
+
+        let img = document.createElement('img');
+        img.classList.add('card-img-top'); 
+        img.src = data.imgUrl; 
+        img.alt = data.name;
+
+        let cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        let text = document.createElement('p');
+        text.classList.add('card-text');
+        text.innerText = `${data.name}`;
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('btn', 'btn-danger');
+        deleteBtn.innerText = 'Delete';
+        
+        deleteBtn.addEventListener('click', () => {
+            fetch(`https://66e7e68bb17821a9d9da6e50.mockapi.io/login/${data.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                }
+            })
+            .then((response) => response.json())
+            .then(() => {
+                container.remove();
+            })
+        });
+
+        cardBody.appendChild(text);
+        cardBody.appendChild(deleteBtn);
+
+        card.appendChild(img);
+        card.appendChild(cardBody);
+
+        container.appendChild(card);
+        imageContainer.appendChild(container);
+    });
+});
